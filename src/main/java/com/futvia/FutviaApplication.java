8779037343d2af4f3dfda8ac34f65257afbbaf4a
Kevin Antonio Lopez Gomez <kalg.antonio.lopez@gmail.com>
2025-06-18 16:56:00 -0600

    Proyecto base Futvia

---
+++ src/main/java/com/futvia/FutviaApplication.java
@@ -0,0 +1,13 @@
+package com.futvia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futvia/FutviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futvia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
